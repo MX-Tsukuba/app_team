c7d19ad612a4793079f3a320e670d6cb55c6e48d
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-04-27 22:10:31 +0900

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <title>
     ようこそ
   </title>
@@ -7,8 +7,8 @@
   </NuxtLink>
   
 </template>
-   -->
-<!-- <script setup>
+   
+ <script setup>
   import useAuthTest from '@/hooks/useAuthTest'
   
   const nuxtApp = useNuxtApp();
@@ -38,10 +38,10 @@
     return () => authData.subscription.unsubscribe();
   }, [session]);
 </script>
-   -->
+  
 
 
-   <script setup>
+   <!-- <script setup>
    import { createClient } from '@supabase/supabase-js'
    const supabase = createClient('https://fuuchzmnathzrsqqsuhg.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1dWNoem1uYXRoenJzcXFzdWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwNjYwNDksImV4cCI6MjAyNTY0MjA0OX0.V9yZTvTcP-1u8zD1PLSKkGhY_fVWLqMDCGBV0hNFBLU')
    const profiles = ref([])
@@ -60,4 +60,4 @@
      <ul>
        <li v-for="profile in profiles" :key="profile.id">{{ profile.username }}</li>
      </ul>
-   </template>
+   </template> -->
--- pages/kazuki.vue
+++ pages/kazuki.vue
@@ -1,5 +1,5 @@
 <script setup="ts">
-import {type Database} from '~/types/database_types';
+import type {Database} from '~/types/database.types';
 const supabase = useSupabaseClient<Database>();
 
 const {data: profiles} = await useAsyncData(async () => {
