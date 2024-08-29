aaa4ac3b24b56167e9d57d1daeca4f2369ea3367
blue031415 <blue031415@gmail.com>
2024-08-29 17:49:18 +0900

    閉じたカードをhtmlとcssで

--- components/scoreDisplay/index.vue
+++ components/scoreDisplay/index.vue
@@ -1,99 +1,74 @@
+
+import logcard from './logcard.vue';
 <template>
-  <section class="wholeScoreDisplay">
-  <div class="logScoreDisplay">
-    <p>ログ</p>
-    <div class="showScoreDisplay">
-      <p class="GolfCourseNameScoreDisplay">筑波大ゴルフ場</p>
-    <p class="toggleDetail" :class="{'isActive': !isActive, 'isInactive': isActive}" @click="toggleDetail">詳細を見る</p>    
-    <p class="toggleDetail" :class="{'isActive': isActive, 'isInactive': !isActive}" @click="toggleDetail">閉じる</p>
-    <ul v-if = "t_sample_kazuki && t_sample_kazuki.length">
-      <li v-for = "sample in t_sample_kazuki" :key="sample.id">
-        <p>H{{ sample.hole || "-" }}, par</p>
-        <div>
-          score: {{ sample.score }}, putts: {{ sample.putts }}
-          <button @click="deletePlayData(sample.id)">x</button>
+  <div class = 'back'>
+    <div class = 'main_container'>
+      <div class = "sub_container">
+        <div class = "month">2024年6月</div>
+        <div class = "subsub_container">
+          <Logcard/>
+          <Logcard/>
+          <Logcard/>
         </div> 
-      </li>    
-    </ul>
-    <p v-else>Loading..</p>
-    </div>    
+      </div>
+      <div class = "sub_container">
+        <div class = "month">2024年5月</div>
+        <div class = "subsub_container">
+
+        </div>
+      </div>
+    </div>
   </div>
-  </section>  
 </template>
 
-<script setup lang="ts">
-import {type Database} from '~/types/database.types';
-const supabase = useSupabaseClient<Database>();
-
-//切り替え
-const isActive = ref(false);
-const toggleDetail = () => {
-  isActive.value = !isActive.value;   
-};
-
-//データ読み込み
-const {data: t_sample_kazuki} = await useAsyncData(async ()=>{
-  const {data} = await supabase.from('t_sample_kazuki').select('*');
-  return data;
-});
-
-//データ削除
-const deletePlayData = async(tableId: number)=> {
-  const { error } = await supabase.from('t_sample_kazuki').delete().match({id: tableId});
-  if(error){
-    alert(error.message);
-  }else if(confirm("Delete this data.")){
-    location.reload();
-    return true;
-  }
-};
-</script>
-
 <style scoped>
-.wholeScoreDisplay {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #FAFAFA;
-}
+  .back{
+    display: flex;
+    width: 100%;
+    min-height: 100vh;
+    padding: 70px 10px;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 32px;
+    background: #F0F0F0;
+  }
 
-.logScoreDisplay {
-  margin-top: 40px;
-  padding: 24px 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 32px;
-  border-radius: 16px;
-  background: #FFF;
-  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.10);
-}
+  .main_container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+  }
 
-.showScoreDisplay{
-  display: flex;
-width: 390px;
-flex-direction: column;
-align-items: center;
-gap: 16px;
-position: relative;
-}
+  .sub_container{
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+  }
 
-.GolfCourseNameScoreDisplay{
-  padding: 2px 8px;
-margin: 0 auto 0 40px;
-  border-radius: 16px;
-border: 1px solid #000;
-}
+  .subsub_container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    align-self: stretch;
+  }
 
-.toggleDetail.isActive{
-  color: #DF0000;
-  font-size: 14px;
-  position: absolute;
-  top: 32px;
-  right: 16px;
-}
-
-.toggleDetail.isInactive{
-  display: none;
-}
+  .month{
+    height: 16px;
+    align-self: stretch;
+    color: #777777;
+    font-family: Inter;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
 </style>
+
+<script setup lang = "ts">
+  import Logcard from "./logcard.vue";
+</script>
---
+++ components/scoreDisplay/logcard.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class = "card_whole">
+        <div class = "date">6月22日 水曜日</div>
+        <div class = "button_fram">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
+                <g filter="url(#filter0_d_351_6002)">
+                    <rect x="8" y="8" width="26.9997" height="26.9997" rx="13.4999" fill="white" shape-rendering="crispEdges"/>
+                    <rect x="8.5" y="8.5" width="25.9997" height="25.9997" rx="12.9999" stroke="#777777" shape-rendering="crispEdges"/>
+                    <path d="M21.1656 22.7533L21.4999 23.054L21.8342 22.7533L26.6335 18.4366L27.4643 19.1852L21.5001 24.5615L15.5349 19.1862L16.3656 18.4374L21.1656 22.7533Z" fill="#777777" stroke="#777777"/>
+                </g>
+                <defs>
+                    <filter id="filter0_d_351_6002" x="0" y="0" width="42.9998" height="43" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset/>
+                    <feGaussianBlur stdDeviation="4"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_351_6002"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_351_6002" result="shape"/>
+                    </filter>
+                </defs>
+                </svg>
+            </div>
+        </div>
+        <div class = "tags">
+            <div class = "score_tag">
+                <div class = "score_text">合計スコア</div>
+                <div class = "score_value">91</div>
+            </div>
+            <div class = "place_tag">
+                <div class = "place_text">📍牛久ゴルフ場</div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .card_whole{
+        display: flex;
+        padding: 16px 0px 16px 16px;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 8px;
+        align-self: stretch;
+        border-radius: 16px;
+        background: #FFF;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        position: relative;
+    }
+
+    .date{
+        color: #777;
+        font-family: Inter;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal; 
+    }
+
+    .tags{
+        display: flex;
+        align-items: center;
+        gap: 11px;
+    }
+
+    .score_tag{
+        display: flex;
+        width: 110px
+        ;
+        height: 24px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 18px;
+        background: #EBEBEB;
+    }
+
+    .score_text{
+        color: #007BE5;
+        font-family: Inter;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+    .score_value{
+        color: #000;
+        font-family: Inter;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+    .place_tag{
+        display: flex;
+        width: 115px;
+        height: 24px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 18px;
+        background: #EBEBEB;
+    }
+
+    .place_text{
+        color: #007BE5;
+        font-family: Inter;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+    .button_fram{
+        display: flex;
+        padding: 6px 9px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        position: absolute;
+        right: 0px;
+        top: 0px;
+    }
+
+</style>
