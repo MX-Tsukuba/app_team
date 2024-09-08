76c00bf7d6e0ce8e3d0daeef5f9ba99e0ace1f42
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-09-08 21:14:22 +0900

    認証部分のスクリプトを追加
    別リポジトリでテストしたところ、コード部分はこれだけでうまく動作しました。
    supabase側にテーブルとトリガーを追加しないといけないので、アクセスできるようになったら追加＆テストしてみます

---
+++ pages/login/login.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// import {signInWithOAuth} from '~/composables/useSupabaseClient';
+
+const supabase = useSupabaseClient()
+
+const signInWithGoogle = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'http://localhost:3000/login/confirm', // ここを絶対パスにしたら成功する
+    }
+  })
+  if (error) console.log(error)
+}
+</script>
+
+<template>
+  <div class="loginContainer">
+    <h1>login.vue</h1>
+    <button class="loginButton" @click="signInWithGoogle">
+      Sign In with Google
+    </button>
+    <NuxtLink class="returnIndex" to="/top">
+      return to index.vue
+    </NuxtLink>
+  </div>
+</template>
