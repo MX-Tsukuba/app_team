25c3f074a35dbc062601e4e8665374c88dc33ad1
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-10-06 17:58:42 +0900

    雑コミット

--- pages/menu/logout.vue
+++ pages/menu/logout.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
+import { useModalStore } from '~/src/store/modal';
+const modalStore = useModalStore();
+const toggleModal = () => modalStore.toggleModal('');
+const supabase = useSupabaseClient();
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+}
+
+import { usePageStore } from '~/src/store/currentPage';
+const pageStore = usePageStore();
+
+const isShowLogoutModal = computed(() => modalStore.isShowModal);
 </script>
 
 <template>
-  <div class="logout">
+  <div class="logout" v-if="isShowLogoutModal">
     <button class="bButton">ログアウト</button>
+  </div>
+  <div class="modal" @click.self="toggleModal">
+    <div class="card">
+    <span class="closeModal" @click="toggleModal">×</span>
+      <p class="title">ログアウトしますか？</p>
+      <div class="buttons">
+        <Nuxt-link @click="toggleModal, signOut" to="../login" class="bButton">確認</Nuxt-link>
+        <button @click="toggleModal" class="bButton">キャンセル</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +38,40 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+.card {
+  width: 70%;
+  margin: 0 auto;
+  padding: 32px;
+  background: #fff;
+  border-radius: 10px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 64px;
+}
+.closeModal {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+.title{
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
