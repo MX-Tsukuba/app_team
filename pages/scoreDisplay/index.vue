67375464321bb3cda8d678c304b3be2749234832
blue031415 <blue031415@gmail.com>
2024-10-12 21:56:27 +0900

    タブのデザインのみ

--- pages/scoreDisplay/index.vue
+++ pages/scoreDisplay/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class = 'back'>
-      <ScoreDisplay/>
+  <div class="back">
+    <div id="tab-container">
+      <div class="label indicator"><p style="opacity: 0.6">スコア一覧</p></div>
+      <div class="label"><p style="opacity: 0.6">動画一覧</p></div>
+    </div>
+    <ScoreDisplay />
   </div>
 </template>
 
 <script setup lang="ts">
 import ScoreDisplay from '~/components/scoreDisplay/logList.vue';
-import { useHeadVarStore } from '~/src/store/headVar.js'
+import { useHeadVarStore } from '~/src/store/headVar.js';
 import { usePageStore } from '~/src/store/currentPage';
 
-const headVarStore = useHeadVarStore()
-headVarStore.title = 'スコア記録'
+const headVarStore = useHeadVarStore();
+headVarStore.title = 'スコア記録';
 const pageStore = usePageStore();
 onMounted(() => {
   pageStore.setCurrentPage('score');
@@ -18,7 +22,7 @@ onMounted(() => {
 </script>
 
 <style scoped>
-  .back{
+.back{
     display: flex;
     background-attachment: fixed;
     min-height: 100vh;
@@ -31,4 +35,35 @@ onMounted(() => {
     align-self: stretch;
     gap: 10px;
   }
+
+#tab-container{
+  display: flex;
+  width: 264px;
+  height: 32px;
+  flex-shrink: 0;
+  border-radius: 9px;
+  background: #DADADB;
+  flex-direction: row;
+}
+
+.label{
+  display: flex;
+  width: 130px;
+  padding: 7px 22px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  color: #000;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.indicator{
+  border-radius: 7px;
+  border: 1px solid rgba(0, 0, 0, 0.04);
+  background: #FFF;
+  box-shadow: 0px 3px 8px 0px rgba(0, 0, 0, 0.12), 0px 3px 1px 0px rgba(0, 0, 0, 0.04);
+}
 </style>
