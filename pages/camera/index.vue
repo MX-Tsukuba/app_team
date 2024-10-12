2390171d11e8c3e5badade703d3027947e1f4aa4
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-10-12 16:50:45 +0900

    ✨動画撮影後にモーダル表示を追加

--- pages/camera/index.vue
+++ pages/camera/index.vue
@@ -5,6 +5,7 @@ definePageMeta({
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
 import { useSupabaseClient } from '#imports';
+import { useModalStore } from '~/src/store/modal';
 
 const video = ref<HTMLVideoElement | null>(null)
 const videoSrc = ref<string | null>(null)
@@ -12,6 +13,12 @@ const mediaRecorder = ref<MediaRecorder | null>(null)
 const recordedChunks: Blob[] = []
 const isRecording = ref(false)
 const router = useRouter()
+
+const modalStore = useModalStore();
+const isShowModal = computed(() => modalStore.isShowModal);
+const modalName = computed(() => modalStore.modalName);
+const toggleModal = (name:string) => modalStore.toggleModal(name);
+
 //Function Definition
 const startCamera = async () => {
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -39,8 +46,8 @@ const startRecording = () => {
       const blob = new Blob(recordedChunks, { type: 'video/mp4' })
       videoSrc.value = URL.createObjectURL(blob)
       recordedChunks.length = 0
-      upLoadSupabaseStorage(blob) 
-    }//ここを後で実行
+      //upLoadSupabaseStorage(blob) 
+    }
     mediaRecorder.value.start()
     isRecording.value = true
   }
@@ -49,8 +56,15 @@ const stopRecording = () => {
   if (mediaRecorder.value && isRecording.value) {
     mediaRecorder.value.stop()
     isRecording.value = false
-    router.push('./preview')
+    toggleModal('confirmModal')
   }
+}
+const confirmVideo = async () => {
+  if (videoSrc.value) {
+    const blob = new Blob(recordedChunks, { type: 'video/mp4' });
+    await upLoadSupabaseStorage(blob);
+  }
+  toggleModal('confirmModal'); // Close modal after confirmation
 }
 const upLoadSupabaseStorage = async (video: Blob | File) => {
   const fileName: string = videoSrc.value ? videoSrc.value.split('/').pop()?.split('.')[0] ?? 'test/test.mp4' : 'test/test.mp4'
@@ -77,7 +91,7 @@ const onFileChange = (event: Event) =>{
     const file = input.files[0];
     videoSrc.value = URL.createObjectURL(file);
     //upLoadSupabaseStorage(file)
-    router.push("./preview")
+    toggleModal('confirmModal')
   }
 }
 
@@ -96,6 +110,16 @@ onBeforeUnmount(() => {
 
 <template>
   <div class="whole">
+    <div v-if="isShowModal && modalName === 'confirmModal'" class="modal" @click.self="toggleModal('confirmModal')">
+      <div class="card">
+        <p class="title">この動画を使用しますか？</p>
+        <div class="buttons">
+          <button @click="toggleModal('confirmModal')" class="cancelButton">キャンセル</button>
+          <Nuxt-link @click="confirmVideo" to="../scoreInput" class="confirmButton">確認</Nuxt-link>
+        </div>
+      </div>
+    </div>
+    
     <img src="~/assets/img/backWhite.png" alt="Left" class="backButton" @click="$router.go(-1)">
     <video ref="video" autoplay playsinline></video>
     <div class="upBtnBox">
@@ -103,7 +127,7 @@ onBeforeUnmount(() => {
         動画を選択
         <input type = "file" accept = "video/*" @change="onFileChange"/>
       </label>
-      <button @click="toggleRecording()"><div :class="isRecording ? 'stopBtn' : 'startBtn'"></div></button>
+      <button @click="toggleRecording()" class="recordingButton"><div :class="isRecording ? 'stopBtn' : 'startBtn'"></div></button>
     </div>
   </div>
 </template>
@@ -154,7 +178,7 @@ video {
   cursor: pointer;
   transition: color 1s, border-color 1s;
 }
-button{
+.recordingButton{
   display: flex;
   width: 64px;
   height: 64px;
@@ -175,5 +199,82 @@ button{
   height: 40px;
   border-radius: 4px;
   background: #F00;
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+.card {
+  display: flex;
+  width: 352px;
+  height: 101px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  flex-shrink: 0;
+  border-radius: 16px;
+  background: #FFF;
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.50);
+}
+.closeModal {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+.title{
+  color: #000;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+.buttons{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+.cancelButton{
+  display: flex;
+  padding: 6px 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 32px;
+  border: 1px solid #E45D5D;
+  background: #FFF;
+  color: rgba(228, 93, 93, 0.75);
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+.confirmButton{
+  display: flex;
+  padding: 5px 55px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 32px;
+  background: #E45D5D;
+  color: #FFF;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 }
 </style>
