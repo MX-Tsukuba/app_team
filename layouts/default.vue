cb70de6abb2eaf1b8c7e4f338be10292f81e528c
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-10-26 23:10:48 +0900

    おｋ

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header/>
+    <div class="backGround">
+        <slot/>
+    </div>
+    <Footer/>
+</template>
+
+<script setup lang="ts">
+import Header from '~/components/bases/header.vue';
+import Footer from '~/components/bases/footer.vue';
+</script>
+<style>
+
+.backGround{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #F0F0F0;
+}
+</style>
