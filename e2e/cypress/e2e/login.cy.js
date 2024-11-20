69438e6bfcf989438e6f577f9c6f294fd35c3072
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-11-20 19:30:20 +0900

    rename spec.cy.js and update

--- e2e/cypress/e2e/login.cy.js
+++ e2e/cypress/e2e/login.cy.js
@@ -1,11 +1,11 @@
 describe('template spec', () => {
   const baseUrl = 'https://localhost:3000/';
   before(() => {
+    cy.viewport(430, 932)
     cy.visit(baseUrl)
-    cy.visit(baseUrl+'top/')
   })
   
   it('login', () => {
-    
+    cy.get('.gsi-material-button-contents').click()
   })
 })
