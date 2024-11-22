fca84496fe15fe0a97a6310afdfa8316f10e117b
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-11-22 23:14:32 +0900

    Update cypress.config.js
    chormeWebSecurityをFalseに設定
    same originでないサイトにも飛べるようになる

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      chromeWebSecurity: false
       // implement node event listeners here
     },
   },
