c7178a1231924c383a540cd5038005fdef16c0bc
toratora-yaki <s2211032@u.tsukuba.ac.jp>
2024-11-25 21:58:51 +0900

    引き継ぎ用のコミット

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -12,27 +12,54 @@ const signInWithGoogle = async () => {
   })
   if (error) console.log(error)
 }
+//実装途中
+async function signInWithEmail() {
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email: 'example@email.com',
+    options: {
+      // set this to false if you do not want the user to be automatically signed up
+      shouldCreateUser: false,
+      emailRedirectTo: 'https://localhost:3000/login/confirm', // ここを絶対パスにしたら成功する
+    },
+  })
+  if (error) console.log(error)
+}
 </script>
 
 <template>
   <div class="loginPage">
     <img class="logo" src="~/assets/img/logo.png">
-    <button class="gsi-material-button" style="width:270" @click="signInWithGoogle">
-      <div class="gsi-material-button-state"></div>
-      <div class="gsi-material-button-content-wrapper">
-        <div class="gsi-material-button-icon">
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
-            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
-            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-            <path fill="none" d="M0 0h48v48H0z"></path>
-          </svg>
+    <div class="loginButtons">
+      <div class="mailLogin">
+        <div class="mailInput">
+          //実装途中
+          <input class="inputBox" type="text" placeholder="メールアドレスを入力"></input>
+          <input class="inputBox" type="text" placeholder="パスワードを入力"></input>
         </div>
-        <span class="gsi-material-button-contents">Googleで続行</span>
-        <span style="display: none;">Googleで続行</span>
+        <button class="mailLoginButton">ログイン</button>
       </div>
-    </button>
+      <div class="or">
+        <hr class="or-lines">
+        <span class="or-text">もしくは</span>
+        <hr class="or-lines">
+      </div>
+      <button class="gsi-material-button" @click="signInWithGoogle">
+        <div class="gsi-material-button-state"></div>
+        <div class="gsi-material-button-content-wrapper">
+          <div class="gsi-material-button-icon">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+              <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+              <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+              <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+              <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+              <path fill="none" d="M0 0h48v48H0z"></path>
+            </svg>
+          </div>
+          <span class="gsi-material-button-contents">Googleで続行</span>
+          <span style="display: none;">Googleで続行</span>
+        </div>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -56,15 +83,90 @@ const signInWithGoogle = async () => {
   align-items: center;
 }
 
-.loginButton {
+.loginButtons {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.mailLogin {
+  display: flex;
+  height: 160px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.mailInput {
+  display: flex;
+  width: 270px;
+  height: 100px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  flex-shrink: 0;
+}
+
+.inputBox {
   display: flex;
   padding: 12px 32px;
-  align-items: flex-start;
+  align-items: center;
   gap: 12px;
-  border-radius: 32px;
+  flex: 1 0 0;
+  align-self: stretch;
+  border-radius: 15px;
   background: #FFF;
+  border: none;
+}
+
+.mailLoginButton {
+  display: flex;
+  height: 42px;
+  padding: 12px 32px;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  flex-shrink: 0;
+  align-self: stretch;
+  border-radius: 32px;
+  background: #E45D5D;
   box-shadow: 0px 2px 4px 0px #000;
-  
+  color: #FFF;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.or {
+  display: flex;
+  height: 40px;
+  padding: 16px 10px 10px 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+}
+
+.or-text {
+  color: rgba(0, 0, 0, 0.50);
+  text-align: center;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.or-lines {
+  background: rgba(0, 0, 0, 0.50);
+  width: 91px;
+  height: 1px;
 }
 
 .gsi-material-button {
@@ -75,14 +177,13 @@ const signInWithGoogle = async () => {
   background-color: WHITE;
   background-image: none;
   -webkit-border-radius: 20px;
-  border-radius: 20px;
+  border-radius: 32px;
   -webkit-box-sizing: border-box;
-  box-sizing: border-box;
   color: #1f1f1f;
   cursor: pointer;
   font-family: inter;
-  font-size: 24px;
-  height: 40px;
+  font-size: 20px;
+  height: 42px;
   /*letter-spacing: 0.25px;*/
   outline: none;
   overflow: hidden;
@@ -100,10 +201,10 @@ const signInWithGoogle = async () => {
 }
 
 .gsi-material-button .gsi-material-button-icon {
-  height: 20px;
+  height: 30px;
   margin-right: 12px;
-  min-width: 20px;
-  width: 20px;
+  min-width: 30px;
+  width: 30px;
 }
 
 .gsi-material-button .gsi-material-button-content-wrapper {
