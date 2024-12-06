630f2cf3e69a464fd368a5f23275b3e4bce580c6
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-12-06 21:47:02 +0900

    Update support\command.js
    セッションを保存してログイン処理を省略しようとしてる(まだ途中)

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login',() => {
+  cy.session([login_id, password], () => {
+    cy.visit(baseurl+'/login')
+    cy.get('[data-test=name]').type(username)
+    cy.get('[data-test=password]').type(password)
+    cy.get('form').contains('Log In').click()
+    cy.url().should('contain', '/login-successful')
+  })
+})
