5c356279397c53c3a6d76bb3b12ab551498052d7
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-12-06 21:46:26 +0900

    edit cypress.config.js
    baseurl, login_id, passwordの定義

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseurl : "https://localhost:3000/",
+    login_id : "reyree41@gmail.com",
+    password : "abc123",
     setupNodeEvents(on, config) {
       chromeWebSecurity: false
       // implement node event listeners here
