f5ee8515eeb2e6fd8a55344fdfdbb3a2d754d049
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-12-09 18:45:26 +0900

    Update test_flow.cy.js
    spec名の変更(template spec → test flow)
    session clearとvisitの順序入れ替え
    入力安定化のためwait(1)の追加
    入力が正しくなされているかの確認
    mealテストへの記述

--- e2e/cypress/e2e/test_flow.cy.js
+++ e2e/cypress/e2e/test_flow.cy.js
@@ -1,19 +1,30 @@
-describe('template spec', {testIsolation: false},() => {
+describe('test flow', {testIsolation: false},() => {
     const baseUrl = 'https://localhost:3000/';
     before(() => {
-        cy.viewport(430, 932);
-        cy.visit(baseUrl);
         // ensure clean test slate for these tests
         cy.then(Cypress.session.clearCurrentSessionData)
+        cy.viewport(430, 932);
+        cy.visit(baseUrl);
+        cy.url().should('include','login');
     });
     
     it('login', () => {
+        // usernameの入力・検証
+        cy.wait(1);
         cy.get('.inputBox').clear().type('reyree41@gmail.com');
+        cy.get('.inputBox').should('have.value','reyree41@gmail.com')
+        // passwordの入力・検証
+        cy.wait(1);
         cy.get('.inputBoxPW').clear().type('abc123');
-        cy.get('.mailLoginButton').focus().click();
+        cy.get('.inputBoxPW').should('have.value','abc123');
+        // ログインボタンを押下
+        cy.get('.mailLoginButton').click();
     });
 
     it('meal', () => {
-        cy.get('[href="/mealInput"]').focus().click();
+        cy.get('[href="/mealInput"]').click();
+        cy.get('.input').clear().type('test1');
+        cy.get('.inputkcal').clear().type(123);
+        cy.get('.addBtn').click;
     });
   });
