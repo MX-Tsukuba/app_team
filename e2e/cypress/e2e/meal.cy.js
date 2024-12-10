1b1aa8fa75887fe20bfbfefc0b75fd36e54c10c3
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-12-10 16:12:50 +0900

    Create [specs].cy.js
    test_flowをいじってるのあんま良くなさそうだから、テスト項目ごとに分離することに

--- e2e/cypress/e2e/meal.cy.js
+++ e2e/cypress/e2e/meal.cy.js
@@ -1,35 +1,24 @@
-describe('test flow', {testIsolation: false},() => {
+describe('meal', () => {
     const baseUrl = 'https://localhost:3000/';
     const login_id = "reyree41@gmail.com";
     const password = "abc123";
-    
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        // returning false here prevents Cypress from
-        // failing the test
-        return false
-    })
-    
+  
     before(() => {
-        // ensure clean test slate for these tests
-        cy.then(Cypress.session.clearCurrentSessionData)
         cy.viewport(430, 932);
         cy.visit(baseUrl);
-        cy.url().should('include','login');
-    });
     
-    it('login', () => {
         // usernameの入力・検証
         cy.wait(1);
-        cy.get('.inputBox').clear().type(login_id);
-        cy.get('.inputBox').should('have.value',login_id)
+        cy.get('.inputBox').clear().type('reyree41@gmail.com');
+        cy.get('.inputBox').should('have.value','reyree41@gmail.com')
         // passwordの入力・検証
         cy.wait(1);
-        cy.get('.inputBoxPW').clear().type(password);
-        cy.get('.inputBoxPW').should('have.value',password);
+        cy.get('.inputBoxPW').clear().type('abc123');
+        cy.get('.inputBoxPW').should('have.value','abc123');
         // ログインボタンを押下
         cy.wait(1);
         cy.get('.mailLoginButton').click();
-        // ログインに成功したか？
+        //ログインに成功したか？
         cy.url().should('eq', baseUrl)
     });
 
@@ -46,7 +35,4 @@ describe('test flow', {testIsolation: false},() => {
         cy.contains('test1').should('exist');
     });
 
-    it.skip('physical', () => {
-        cy.get('[href="/bodyInput"]').click();
-    })
   });
---
+++ e2e/cypress/e2e/physical.cy.js
@@ -0,0 +1,28 @@
+describe('physical', () => {
+    const baseUrl = 'https://localhost:3000/';
+    const login_id = "reyree41@gmail.com";
+    const password = "abc123";
+  
+    before(() => {
+        cy.viewport(430, 932);
+        cy.visit(baseUrl);
+    
+        // usernameの入力・検証
+        cy.wait(1);
+        cy.get('.inputBox').clear().type('reyree41@gmail.com');
+        cy.get('.inputBox').should('have.value','reyree41@gmail.com')
+        // passwordの入力・検証
+        cy.wait(1);
+        cy.get('.inputBoxPW').clear().type('abc123');
+        cy.get('.inputBoxPW').should('have.value','abc123');
+        // ログインボタンを押下
+        cy.wait(1);
+        cy.get('.mailLoginButton').click();
+        //ログインに成功したか？
+        cy.url().should('eq', baseUrl)
+    });
+
+    it.skip('physical', () => {
+        cy.get('[href="/bodyInput"]').click();
+    })
+  });
