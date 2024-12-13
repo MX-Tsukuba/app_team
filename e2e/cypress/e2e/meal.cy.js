fbf432146b34c843ab87922484432e2a158ae25f
Rintaro Hisa <tofu.rintarohisa@gmail.com>
2024-12-13 20:26:50 +0900

    Update meal.cy.js
    ・細部の変更(主にcy.wait())
    ・入力した食事内容の結果表示について検証

--- e2e/cypress/e2e/meal.cy.js
+++ e2e/cypress/e2e/meal.cy.js
@@ -14,7 +14,7 @@ describe('meal', () => {
         cy.visit(baseUrl);
     
         // usernameの入力・検証
-        cy.wait(1);
+        cy.wait(100);
         cy.get('.inputBox').clear().type('reyree41@gmail.com');
         cy.get('.inputBox').should('have.value','reyree41@gmail.com')
         // passwordの入力・検証
@@ -25,13 +25,15 @@ describe('meal', () => {
         cy.wait(1);
         cy.get('.mailLoginButton').click();
         //ログインに成功したか？
-        cy.url().should('eq', baseUrl)
+        cy.url().should('eq', baseUrl);
     });
 
     it('meal', () => {
         // 「食事を記録」を押下
         cy.get('[href="/mealInput"]').focus().click();  // クリック
         cy.url().should('include','mealInput');         // URLの確認
+        cy.wait(100);
+        const today_date = cy.get('.changeDate > :nth-child(1)');
 
         // 今日の朝ごはん1を入力
         const test1_kcal = Math.floor(Math.random()*100);   // (0,100)の整数をランダムに生成
@@ -43,21 +45,39 @@ describe('meal', () => {
         cy.get('.resultTable').contains(test1_kcal).should('exist');
 
         // 今日の朝ごはん2を入力
+        const test_num = Math.floor(Math.random()*100000);
         const test2_kcal = Math.floor(Math.random()*100);   // (0,100)の整数をランダムに生成
-        cy.get('.input').clear().type('teeeeeeeeeeeeeeeeeeeeeest2');             // 「食事名」に'test1'を入力
+        cy.get('.input').clear().type('test'+String(test_num));             // 「食事名」に'test1'を入力
         cy.get('.inputkcal').clear().type(test2_kcal);      // 「カロリー」にtest1_kcalを入力
         cy.get('.addBtn').click();                          // 「追加」ボタンを押下
         // 入力内容の確認
-        cy.get('.resultTable').contains('teeeeeeeeeeeeeeeeeeeeeest2').should('exist');
+        cy.get('.resultTable').contains('test'+String(test_num)).should('exist');
         cy.get('.resultTable').contains(test2_kcal).should('exist');
-        
+
         // スクリーンショットの保存
-        cy.scrollTo('bottom' ,{ duration: 200 })
-        cy.wait(300)
-        cy.get('header').invoke('css','position','absolute')
-        //cy.get('.footer').invoke('css','position','absolute')
-        cy.screenshot('朝ごはん入力', {overwrite: true})
-        cy.get('header').invoke('css','position',null)
-        //cy.get('.footer').invoke('css','position',null)
+        /*
+        cy.scrollTo('bottom' ,{ duration: 200 });
+        cy.wait(300);
+        cy.get('header').invoke('css','position','absolute');
+        cy.get('.footer').invoke('css','position','absolute');
+        cy.screenshot('朝ごはん入力', {overwrite: true});
+        cy.get('header').invoke('css','position',null);
+        cy.get('.footer').invoke('css','position',null);
+        */
+
+        // 結果表示の確認
+        cy.url().should('include','mealInput');
+        cy.get('.insertBtn').click();                                   // 結果挿入ボタンを押下
+        cy.url().should('include','mealDisplay');     // URLの検証
+        cy.get(':nth-child(1) > .dateBtn').click();                     // 日付を今日のに
+        cy.get('.dayfont').should('eq',today_date);                     // 日付が今日のか確認(仕様変わってたら修正)
+        cy.get('.true-class > img').click();                            // 朝ごはんボタンを押下
+        cy.get('.table').contains('test'+test_num).should('exist');     // 2つ目の朝ごはんが正しい名前で保存されてるか
+        cy.get('.backButton').click();                                  // 入力画面へ戻る
+
+        // 過去の食事を記録する
+        //cy.url().should('include','mealInput');
+        //cy.get('.changeDate > :nth-child(1)').click();
+
     });
   });
